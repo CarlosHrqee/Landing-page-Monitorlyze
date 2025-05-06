cc480a1c6395a7fa6dc7018125da6dd2f5a3b632
CarlosHrqee <159045240+CarlosHrqee@users.noreply.github.com>
2025-05-06 11:21:12 -0400

    Create posts.ts

---
+++ src/lib/posts.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(import.meta.env.BASE_URL || "", "src/content/posts");
+
+export function getAllPosts() {
+  const filenames = fs.readdirSync(postsDirectory);
+  const posts = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data } = matter(fileContents);
+    return {
+      ...data,
+    };
+  });
+
+  return posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+}
+
+export function getPostBySlug(slug: string) {
+  const filePath = path.join(postsDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  const { data, content } = matter(fileContents);
+  return { data, content };
+}
