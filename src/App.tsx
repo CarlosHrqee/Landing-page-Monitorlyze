b797670a407585554dc9d97275296bf423cc51d5
CarlosHrqee <159045240+CarlosHrqee@users.noreply.github.com>
2025-05-06 11:53:58 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import BlogList from "./pages/blog";
-import BlogPost from "./pages/blog/[slug]";
 
 const queryClient = new QueryClient();
 
